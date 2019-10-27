5a7d731ea624711ecc98d1ee00c0d6ec436d5c30
MMMMMMoSky <q94354906@gmail.com>
2019-10-27 18:52:20 +0800

    添加了可变参数所需的宏

--- macro_def.h
+++ macro_def.h
@@ -2,6 +2,23 @@
 #define MACRO_DEF_H
 
 typedef unsigned int uint;
+typedef unsigned char byte;
+
+// define about "va" is for printf
+typedef char *va_list;
+
+/* Amount of space required in an argument list for an arg of type TYPE.
+   TYPE may alternatively be an expression whose type is used.  */
+
+#define __va_rounded_size(TYPE) \
+    (((sizeof(TYPE) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+
+#define va_start(AP, LASTARG) \
+    (AP = ((char *)&(LASTARG) + __va_rounded_size(LASTARG)))
+
+#define va_arg(AP, TYPE) \
+    (AP += __va_rounded_size(TYPE), *((TYPE *)(AP - __va_rounded_size(TYPE))))
+
 
 // VGA 80x25x16 text mode
 #define VIDEO_MEM 0xB8000
