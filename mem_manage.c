43b75b10a49030a69fa6eb37f7a2118fe8748433
czy23333 <554550216@qq.com>
2019-10-28 23:37:03 +0800

    添加了两个主要的函数mem_alloc和mem_free以及一些调试或者辅助的函数， 初步测试是可以使用的（毕竟基本上是抄过来的^_^），内存管理部分在教程中还有一些其他的函数没有实现，但是那些函数现在基本上用不到，所以暂时先不添加，到了要用的时候再添加，下一步准备进军进程管理，只有有了进程管理内存管理才有意义

--- mem_manage.c
+++ mem_manage.c
@@ -64,7 +64,7 @@ void mem_calc(void)
 
     for(i = 0; i < PAGING_PAGES; i++)
         if(!mem_map[i]) free++;
-    printf("%d pages free (of %d in total)\n", i, PAGING_PAGES);
+    printf("%d pages free (of %d in total)\n", free, PAGING_PAGES);
     
     // 遍历除了页表页目录的其余页表项, 如果页面有效, 则统计有效页面数量
     for(i = 2; i < 1024; i++) {
@@ -78,6 +78,7 @@ void mem_calc(void)
             printf("PageDir[%d] uses %d pages\n", i, k);
         } 
     }
+    printf("\n");
     return ;
 }
 
@@ -114,14 +115,190 @@ void mem_freepage(unsigned int addr)
 	printf("trying to free free page");
 }
 
+/*
+ * 以下函数用于内核中的内存分配与释放(参考linux0.11mallo库函数))
+ * 这个算法还有点小复杂！
+*/
+#define PAGE_SIZE 4096
+struct bucket_desc *free_bucket_desc = (struct bucket_desc*) 0 ;
+
+typedef struct _bucket_dir s_bucket_dir;
+static struct _bucket_dir bucket_dir[10];
+
+//用于初始化一页用于分配桶
+void init_bucket()
+{
+    struct bucket_desc *bdesc, *first;
+    bdesc = first = (struct bucket_desc *)mem_getfreepage();
+    if(!bdesc)
+    {
+        printf("no idle page!");
+        return;
+    }
+    for(int i = PAGE_SIZE/sizeof(struct bucket_desc); i > 1; i--)
+    {
+        bdesc->next = bdesc+1;
+        bdesc++;
+    }
+    bdesc->next = free_bucket_desc;
+    free_bucket_desc = first;
+}
+
+
+void *mem_alloc(unsigned int len)
+{
+    struct bucket_desc *bdesc;
+    struct _bucket_dir *bdir;
+    void *ret;
+    for (bdir = &bucket_dir[0]; bdir->size ; bdir++)
+    {
+        if(bdir->size >= len) 
+        {
+            break;
+        }  
+    }
+    if(!bdir->size)
+    {
+        printf("try to allocate too much memory!");
+        return;
+    }
+
+    io_cli();
+    for(bdesc=bdir->chain; bdesc; bdesc=bdesc->next)
+        if(bdesc->freeptr)
+            break;
+    
+    if(!bdesc)
+    {
+        char *cp;
+        
+        if(!free_bucket_desc)
+            init_bucket();
+            
+        bdesc = free_bucket_desc;
+        free_bucket_desc = bdesc->next;
+        bdesc->bucket_size = bdir->size;
+        bdesc->refcnt = 0;
+        bdesc->page = bdesc->freeptr = cp = mem_getfreepage();
+
+        if(!bdesc->page)
+        {
+            printf("no idle pages!");
+        } 
+        for(int i = 1; i < PAGE_SIZE/bdir->size; i++)
+        {
+            *((char **) cp) = cp + bdir->size;
+            cp += bdir->size;
+        }
+
+        *((char **) cp) = 0;
+        bdesc->next = bdir->chain;
+        bdir->chain = bdesc;
+    }
+    ret = bdesc->freeptr;
+    bdesc->freeptr = *((char **)ret);
+    bdesc->refcnt++;
+    io_sti();
+    return ret;
+}
+
+void *mem_free(void *obj, int size)
+{
+    struct _bucket_dir *bdir;
+    struct bucket_desc *bdesc, *prev;
+    for(bdir = bucket_dir; bdir->size; bdir++)
+    {
+        prev = 0;
+        if(bdir->size < size) continue;
+        for(bdesc = bdir->chain; bdesc; bdesc++)
+        {
+            if(bdesc->page = obj)
+                goto found;
+            prev = bdesc;
+        }
+    }
+    printf("bad address!");
+found:
+    io_cli();
+    *((char **)obj) = bdesc->freeptr;
+    bdesc->freeptr = obj;
+    bdesc->refcnt--;
+    if(!bdesc->refcnt)
+    {
+        if(prev->next != bdesc)
+        {
+            for(prev = bdir->chain; prev; prev=prev->next)
+                if(prev->next == bdir)break;
+
+            if(prev)
+                prev->next = bdesc->next;
+            else {
+                if (bdir->chain != bdesc)
+                    printf("malloc bucket chains corrupted");
+                bdir->chain = bdesc->next;
+		    }
+        }
+        mem_freepage((unsigned int)bdesc->page);
+        bdesc->next = free_bucket_desc;
+        free_bucket_desc = bdesc;
+    }
+}
+
+void mem_print_freebucket()
+{
+    int i = 0;
+    struct bucket_desc *bdesc = free_bucket_desc;
+    while(bdesc!=0)
+    {
+        printf("%d ",i++);
+        bdesc = bdesc->next;
+    }
+}
+
+void mem_print_bucketdir()
+{
+    for(int i = 0;i<9 ;i++)
+    {
+        printf("%d:",bucket_dir[i].size);
+        for(struct bucket_desc* bdesc = bucket_dir[i].chain; bdesc; bdesc=bdesc->next)
+        {   
+            printf("(%x %d %x) ",bdesc->page, bdesc->refcnt, bdesc->freeptr);
+        }
+        printf("\n");
+    }
+}
+
+void mem_initbdir()
+{
+    int sz = 16;
+    for(int i = 0;i<10;i++)
+    {
+        bucket_dir[i].size = 1<<(i+4);
+        bucket_dir[i].chain = (struct bucket_desc*) 0;
+    }
+    bucket_dir[9].size = 0;
+    return ;
+}
+
+// 用于测试内存管理函数
 void mem_functest(void)
 {
-    mem_init(0x100000,0x0898f00);
-    long i = mem_getfreepage();
-    //printf("%d",mem_map[1944]);
-    mem_printmap();
-    mem_freepage(i);
-    mem_printmap();
+    mem_init(0x100000,0x0898f00);   
+    mem_initbdir();
+    void * addr = mem_alloc(100);
+    void * addr2 = mem_alloc(1000);
+    void * addr3 = mem_alloc(512);
+    void * addr4 = mem_alloc(10);
+    void * addr5 = mem_alloc(48);
+    
+    mem_print_bucketdir();
+    mem_free(addr, 0);
+    mem_free(addr2, 0);
+    mem_free(addr3, 0);
+    mem_free(addr4, 0);
+    mem_free(addr5, 0);
+    
+    mem_print_bucketdir();
 loop:
     for(;;);
 }
