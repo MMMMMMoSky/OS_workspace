29e68a74e9f1ec687eb10de2fe46040f71664ceb
MMMMMMoSky <q94354906@gmail.com>
2019-11-06 15:46:08 +0800

    finish cmd_num_conv

--- builtin_commands.c
+++ builtin_commands.c
@@ -14,3 +14,170 @@ void cmd_clear(const char *param)
 {
     v_clear();
 }
+
+void cmd_num_conv_help()
+{
+    prints(
+        "\n"
+        "num-conv\n"
+        "  convert number base\n"
+        "Usage: num-conv [options] number\n"
+        "Options:\n"
+        "  -f, -from    specify origin base of number (default 10)\n"
+        "  -t, -to      specify target base (default 10)\n"
+        "  -h, -help    print this page\n"
+        "number: a positive integer\n"
+        "Notice: the base must be in the range of [2, 16]\n"
+        "\n"
+    );
+}
+
+uint parse_digit(char c)
+{
+    if ('0' <= c && c <= '9') {
+        return c - '0';
+    }
+    if ('a' <= c && c <= 'f') {
+        return c - 'a' + 10;
+    }
+    if ('A' <= c && c <= 'F') {
+        return c - 'A' + 10;
+    }
+    return 0xffffffffu;
+}
+
+void convert_num_base(uint number, uint target_base, char *result)
+{
+    if (number == 0) {
+        result[0] = '0';
+        result[1] = 0;
+        return ;
+    }
+
+    uint len = 0;
+    while (number) {
+        uint digit = number % target_base;
+        result[len++] = digit + (digit < 10 ? '0' : 'A' - 10);
+        number /= target_base;
+    }
+    result[len] = 0;
+
+    for (uint l = 0, r = len - 1; l < r; l++, r--) {
+        char tmp = result[l];
+        result[l] =result[r];
+        result[r] = tmp;
+    }
+}
+
+// taboo: I just use goto when syntax error occurred
+void cmd_num_conv(const char *param)
+{
+    const char *p = param;
+    uint from = 0, to = 0;
+
+    // FOR DEBUG
+    // printf("parsing options...");
+
+    // parse options
+    while (1) {
+        while (*p && *p != '-') p++;  // find '-'
+        if (*p == 0) break;
+        p++;
+        if (strncmp(p, "h", 1) == 0 || strncmp(p, "help", 4) == 0) {
+            cmd_num_conv_help();
+            return ;
+        }
+        else if (strncmp(p, "f ", 2) == 0 || strncmp(p, "from ", 5) == 0) {
+            if (from) {
+                prints("Error: -f can be used at most once!\n");
+                goto cmd_num_conv_bad_syntax;
+            }
+            // parse -f
+            while (*p != ' ') p++;
+            p++;
+            while (*p && *p != ' ') {
+                if (*p < '0' || *p > '9') {
+                    goto cmd_num_conv_base_out_of_range;
+                }
+                from = from * 10 + (*p) - '0';
+                p++;
+            }
+            if (from < 2 || from > 16) {
+                goto cmd_num_conv_base_out_of_range;
+            }
+        }
+        else if (strncmp(p, "t ", 2) == 0 || strncmp(p, "to ", 3) == 0) {
+            if (to) {
+                prints("Error: -t can be used at most once!\n");
+                goto cmd_num_conv_bad_syntax;
+            }
+            // parse -t
+            while (*p != ' ') p++;
+            p++;
+            while (*p && *p != ' ') {
+                if (*p < '0' || *p > '9') {
+                    goto cmd_num_conv_base_out_of_range;
+                }
+                to = to * 10 + (*p) - '0';
+                p++;
+            }
+            if (to < 2 || to > 16) {
+                goto cmd_num_conv_base_out_of_range;
+            }
+        }
+        else {
+            prints("Error: invalid option\n");
+            goto cmd_num_conv_bad_syntax;
+        }
+    }
+
+    // FOR DEBUG
+    // printf("ok\nparsing number...");
+
+    // default base
+    if (from == 0) from = 10;
+    if (to == 0) to = 10;
+
+    // parse number
+    p = param;
+    while (*p) {
+        while (*p && *p == ' ') p++;
+        if (*p == '-') {
+            while (*p && *p != ' ') p++;
+            p++;
+            while (*p && *p != ' ') p++;
+        }
+        else break;
+    }
+    if (*p == 0) {
+        prints("Error: please input number\n");
+        goto cmd_num_conv_bad_syntax;
+    }
+    uint number = 0;
+    while (*p && *p != ' ') {
+        uint digit = parse_digit(*p);
+        if (digit >= from) {
+            printf("Error: invalid number in %u-base.\n", from);
+            goto cmd_num_conv_bad_syntax;
+        }
+        number = number * from + digit;
+        p++;
+    }
+
+    // FOR DEBUG
+    // printf("ok\nconverting number...\n");
+
+    // convert number
+    char res[64];
+    convert_num_base(number, to, res);
+    prints(res);
+    printc('\n');
+    return ;
+
+    cmd_num_conv_base_out_of_range:
+    prints("Error: base must be in range of [2, 16]\n");
+    goto cmd_num_conv_bad_syntax;
+
+    cmd_num_conv_bad_syntax:
+    prints("Try 'num-conv -h' for more information\n");
+}
